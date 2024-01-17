ccf251ae82bd02a576d81f4f76c71b207c30bcbd
ThakareSakshi <sakshithakare121@gmail.com>
2024-01-17 21:04:27 +0530

    updated with toalprice

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import {ShoppingList} from './context/ShoppingItemsContext';
 
 function App() {
   return (
-    <div className="App">
-     <h1 className='font-bold text-4xl'>Shopping List</h1>
+    <div className="App p-4">
+     <h1 className='font-bold text-4xl m-4 '>Shopping List</h1>
   
   <ShoppingList>
  <div className='flex gap-3 w-full '>
--- src/Components/CardItems.js
+++ src/Components/CardItems.js
@@ -1,32 +1,39 @@
-import React from 'react'
-import { useContext } from 'react'
-import ShoppingItemContext from '../context/ShoppingItemsContext'
+import React from "react";
+import { useContext, useState } from "react";
+import ShoppingItemContext from "../context/ShoppingItemsContext";
 
 const CardItems = (props) => {
+  const ItemList = useContext(ShoppingItemContext);
 
-    const ItemList=useContext(ShoppingItemContext);
+  const cartList = ItemList.data.filter((item) => item.count > 0);
+  const [totalPrice, setTotalPrice] = useState(0);
+  let temp=totalPrice;
+
+
   return (
-    <div className='p-6 border-solid border-gray-100 border-2 bg-gray-200 w-1/2'>
-        <h2 className='font-bold '>Added to Cart</h2>
-      {
-      ItemList.data.map((prod)=>{
-        
-         if(prod.count>0){
-            return  <div className='border-2 border-solid m-2 flex justify-between p-4 item-center bg-white shadow-sm '>
-            <div>
+    <div className="p-6 border-solid border-gray-100 border-2 bg-gray-200 w-1/2 relative">
+      <h2 className="font-bold ">Added to Cart</h2>
+      {cartList.length != 0 ? (
+        cartList.map((prod) => {
+          temp+=(prod.count*prod.price);
+          return (
+            <div className="border-2 border-solid m-2 flex justify-between p-4 item-center bg-white shadow-sm ">
+              <div>
                 <h4>{prod.name}</h4>
-               
+              </div>
+              <div className="flex gap-4 rounded-full px-4 items-center  text-md">
+                {prod.count} x {prod.price}
+              </div>
             </div>
-            <div className='flex gap-4 rounded-full px-4 items-center  text-md' >
-             {prod.count} x {prod.price}
-            </div>
-        
-            </div>
-         }
-       })
-      }
+          );
+        })
+      ) : (
+        <div>no product added to cart</div>
+      )}
+    
+      <div className="absolute bottom-1 p-2 bg-white w-full">total Price:{temp} Rs.</div>
     </div>
-  )
-}
+  );
+};
 
-export default CardItems
+export default CardItems;
